716b3e5149e6d2c5b89b445247c75c60fbfc954e
Jiangx <994898372@qq.com>
2019-04-15 13:53:50 +0800

    .

--- src/renderer/utils/request.js
+++ src/renderer/utils/request.js
@@ -7,62 +7,19 @@ const service = axios.create({
   // baseURL: 'http://10.30.11.6:8080',
   // baseURL: 'http://10.30.18.62:1010',
   baseURL: 'http://10.88.66.143:13302/jsonrpc',
-  //  baseURL: 'http://142.10.179.220:13302/jsonrpc',
+  // baseURL: 'http://142.10.179.220:13302/jsonrpc',
   // baseURL: process.env.BASE_API, // api 的 base_url
   timeout: 5000 // request timeout
 })
 service.interceptors.request.use(
-  // (config) => {
-  //   console.log(config)
-  //   return config
-  // }
+  config => { return config }
 )
 
 // response interceptor
 service.interceptors.response.use(
-  /**
-   * 下面的注释为通过在response里，自定义code来标示请求状态
-   * 当code返回如下情况则说明权限有问题，登出并返回到登录页
-   * 如想通过 xmlhttprequest 来状态码标识 逻辑可写在下面error中
-   * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
-   */
-  // response => {
-  //   const res = response.data
-  //   if (res.code !== 20000) {
-  //     Message({
-  //       message: res.message,
-  //       type: 'error',
-  //       duration: 5 * 1000
-  //     })
-  //     // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-  //     if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-  //       // 请自行在引入 MessageBox
-  //       // import { Message, MessageBox } from 'element-ui'
-  //       MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-  //         confirmButtonText: '重新登录',
-  //         cancelButtonText: '取消',
-  //         type: 'warning'
-  //       }).then(() => {
-  //         store.dispatch('FedLogOut').then(() => {
-  //           location.reload() // 为了重新实例化vue-router对象 避免bug
-  //         })
-  //       })
-  //     }
-  //     return Promise.reject('error')
-  //   } else {
-  //     return response.data
-  //   }
-  // },
+  response => { return response },
 
-  response => {
-    if (response) {
-      return response
-    } else {
-      console.log('sdfsdfsdf')
-    }
-  },
   error => {
-    console.log(error) // for debug
     return Promise.reject(error)
   }
 )
