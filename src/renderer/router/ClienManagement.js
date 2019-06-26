c8e4297df08a6dc1a916cb42826796efccb1560d
Jiangx <994898372@qq.com>
2019-06-26 10:59:52 +0800

    ..

--- src/renderer/router/ClienManagement.js
+++ src/renderer/router/ClienManagement.js
@@ -5,27 +5,27 @@ export default {
   path: '/type3',
   name: '3',
   component: Layout,
-  redirect: '/type3/subType1/subType1-1',
+  redirect: '/ClientManagement/disklesslist',
   meta: { title: 'ClientManagement', icon: 'example' },
   children: [
     {
-      path: 'subType1',
+      path: 'ClientManagement',
       name: 'type3-SubType1',
       meta: { title: 'ClientManagement', hidden: true },
       component: NullLayout,
       children: [
         {
-          path: 'subType1-1',
+          path: 'disklesslist',
           name: 'type3-SubType1-1',
           alias: '/disklesslist',
           meta: { title: 'ClientList' },
-          component: () => import('@/views/type3/subtype1/subtype1-1')
+          component: () => import('@/views/ClientManagement/ClientManagement/ClientList')
         },
         {
           path: 'subType1-add',
           name: 'type3-SubType1-add',
           meta: { title: 'ClientSetting' },
-          component: () => import('@/views/type3/subtype1/subtype1-add'),
+          component: () => import('@/views/ClientManagement/ClientManagement/subtype1-add'),
           hidden: true
         }
       ]
@@ -41,19 +41,19 @@ export default {
           path: 'subType2-1',
           name: 'type3-SubType1-1',
           meta: { title: 'BaseSetting' },
-          component: () => import('@/views/type3/subtype1/BaseSetting')
+          component: () => import('@/views/ClientManagement/ClientSetting/BaseSetting')
         },
         {
           path: 'subType2-2',
           name: 'type3-SubType1-2',
           meta: { title: 'HomeScreenIcon' },
-          component: () => import('@/views/type3/subtype1/HomeScreenIcon')
+          component: () => import('@/views/ClientManagement/ClientSetting/HomeScreenIcon')
         },
         {
           path: 'subType2-3',
           name: 'type3-SubType1-3',
           meta: { title: 'BootBatch' },
-          component: () => import('@/views/type3/subtype1/BootPath')
+          component: () => import('@/views/ClientManagement/ClientSetting/BootPath')
         }
       ]
     }
