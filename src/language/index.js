a9e18861c413222060755639e2267b44c45f3d18
Jiangx <994898372@qq.com>
2019-06-27 12:30:20 +0800

    .

--- src/language/index.js
+++ src/language/index.js
@@ -1,38 +1,32 @@
-/**
- * 引入vue-i18n
- */
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-/**
- * 导入iview自带语言包
- */
-import zh from 'iview/dist/locale/zh-CN'
 import en from 'iview/dist/locale/en-US'
-import zhtw from 'iview/dist/locale/zh-TW'
-// /**
-//  * 导入APP json语言包
-//  */
-import Zh from './zh-CN.json'
-import En from './en-US.json'
-import ct from './CT.json'
+import zh from 'iview/dist/locale/zh-CN'
+import zhTw from 'iview/dist/locale/zh-TW'
+
+import zhCn from './zh-CN.json'
+import enUs from './en-US.json'
+import zhTW from './CT.json'
+
 Vue.use(VueI18n)
-
-// // 自动设置语言
-// // 获取本机系统语言
 const navLang = navigator.language
+const lang = localStorage.getItem('lang') || navLang || 'en-US'
 
-// const localLang = (navLang === 'zh-CN' || navLang === 'en-US' || navLang === 'zh-TW') ? navLang : 'en-US'
-const localLang = localStorage.getItem('language') || navLang || 'en-US'
-
+Vue.locale = () => {}
 const messages = {
-  'zh-CN': Object.assign(Zh, zh),
-  'en-US': Object.assign(En, en),
-  'zh-TW': Object.assign(zhtw, ct)
+  'en-US': { ...enUs, ...en },
+  'zh-CN': { ...zhCn, ...zh },
+  'zh-TW': { ...zhTw, ...zhTW }
 }
 
+if (!localStorage.getItem('lang')) {
+  localStorage.setItem('lang', lang)
+}
 const i18n = new VueI18n({
-  locale: localLang, // 设置地区
-  messages // 设置地区信息
+  locale: lang, // set locale
+  messages, // set locale messages
+  silentFallbackWarn: true
 })
+
 export default i18n
